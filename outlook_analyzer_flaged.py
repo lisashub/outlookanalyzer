53ed4447840260d31865c2bb3531cd0d09e5c45e
cplummer-uwm <cplummer@uwm.edu>
2022-04-03 17:40:24 -0500

    Update outlook_analyzer_flaged.py
    Updated code with error handling to handle no flagged emails and ignore flagged emails marked as complete. Also added code to open image at end.

--- outlook_analyzer_flaged.py
+++ outlook_analyzer_flaged.py
@@ -1,11 +1,13 @@
 #Example Script for identifying flagged emails with a visualization
+# There are many ways to export data frame as an image: https://stackoverflow.com/questions/35634238/how-to-save-a-pandas-dataframe-table-as-a-png/
+# This might not be the best solution
 
 import win32com.client #core extraction library
 from tqdm import tqdm # library to display extraction progress bar
 import pandas as pd # library to tabulate data and generate plot/image
 import numpy as np
 import dataframe_image as dfi
-
+from PIL import Image #library to display image (temporary if we build web frontend
 
 #Assign Extraction Variables
 outlook = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI") #maps outlook variable to outlook application
@@ -24,7 +26,10 @@ i = 0 # counter variable to help count collected senders
 print("Retrieving Messages:")
 for item in tqdm(messages):
 
-    if (item.FlagRequest == "Follow up"):
+# https://devblogs.microsoft.com/scripting/how-can-i-determine-the-follow-up-status-of-outlook-emails/
+# FlagStatus 1 is completed
+# FlagStatus 2 is Marked for follow-up
+    if (item.FlagRequest == "Follow up" and item.FlagStatus == 2):
         i += 1 # counter incrementer
 
         # Create dict object
@@ -52,11 +57,27 @@ for item in tqdm(messages):
 #Prints some ouputs to Command Line
 print("\n")
 print("Number of Flagged emails: " , i)
-print(flagged_messages_list)
+# print(flagged_messages_list)
 
-# Create a dataframe from the list of dictionaries
-df = pd.DataFrame(flagged_messages_list)
+number_of_flagged_follow_up_messages = i
 
-df_styled = df.style.background_gradient() #adding a gradient based on values in cell
-# Export the data frame as an image
-dfi.export(df_styled,"flagged_email_list.png")
+if (number_of_flagged_follow_up_messages > 0):
+
+    try:
+
+        # Create a dataframe from the list of dictionaries
+        df = pd.DataFrame(flagged_messages_list)
+    except Exception as e:
+        print("error when creating data frame:" + str(e))
+        
+
+    try:
+
+        df_styled = df.style.background_gradient() #adding a gradient based on values in cell
+        # Export the data frame as an image
+        dfi.export(df_styled,"flagged_email_list.png")
+        im = Image.open("flagged_email_list.png") #displays plot in default photo viewer; can be moved to web app
+        im.show()
+
+    except Exception as e:
+        print("error when exportin data frame:" + str(e))
