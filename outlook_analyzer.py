a41dac5777798201e0501705cecbe7847819610d
cplummer-uwm <cplummer@uwm.edu>
2022-04-29 01:02:06 -0500

    Update outlook_analyzer.py
    Fix for the long exchange text for sender email address for meeting responses - since there is no email attribute using the sender name

--- outlook_analyzer.py
+++ outlook_analyzer.py
@@ -313,6 +313,8 @@ def return_sender(outlook_object):
             sender = outlook_object.Sender.GetExchangeUser().PrimarySmtpAddress
         else:
             sender = outlook_object.SenderEmailAddress
+    elif outlook_object.Class == 56 or outlook_object.Class == 53: #Class "53" and "56" are responses to meeting invites
+            sender = outlook_object.SenderName
     else:
         sender = outlook_object.SenderEmailAddress
 
